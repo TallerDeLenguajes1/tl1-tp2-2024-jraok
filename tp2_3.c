6e3bef7c61acaaa1ee378345a095d6a80045f21c
Joel Román Arancibia <joelromanarancibia@gmail.com>
2024-04-10 02:58:42 -0300

    correcciones de la aritmetica de puntero

--- tp2_3.c
+++ tp2_3.c
@@ -8,13 +8,13 @@ int main(){
 
     int i,j;
     int mt[N][M];
-    for(i = 0;i<N; i++){
-        for(j = 0;j<M; j++){
-            mt[i][j]=1+rand()%100;
-            printf("%lf", mt[i][j]);
+    
+    for(i = 0; i < N; i++){
+        for(j = 0; j < M; j++){
+            *(*(mt + i)+j) = 1 + rand()%100;
+            printf("%d\t", *(*(mt + i)+j));
         }
         printf("\n");
     }
     return 0;
 }
-
