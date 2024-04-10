0c9d1971cec881c9c8ac92bc20120489174a7508
Joel Román Arancibia <joelromanarancibia@gmail.com>
2024-04-10 02:22:15 -0300

    Trabajo Practico terminado

--- tp2_4.c
+++ tp2_4.c
@@ -10,8 +10,9 @@ struct computadora{
 }typedef PC;
 
 PC cargarPc();
-
-
+void mostrarLista(PC *lista);
+void masVieja(PC *lista);
+void cuchau(PC *lista);
 
 int main(){
     puts("\n\n\t\t------CARGANDOOO-----");
@@ -21,33 +22,68 @@ int main(){
         *(lista + i)= cargarPc();
     }
     
+    mostrarLista(lista);   
+    masVieja(lista);
+    cuchau(lista);
 
+    for (int i = 0; i < M; i++){
+        free(lista[i].tipo_cpu);
+    }
+    free(lista);
 }
 
 PC cargarPc(){
     PC compu;
     char tipos[6][10]={"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
     compu.tipo_cpu = (char*)malloc(M*sizeof(char));
-    int indice;
+    int indice = rand()%6;
     compu.anio = 2015 + rand()%9;
     compu.cantidad = 1 + rand()%8;
     compu.velocidad = 1 + rand()%3;
-    do{ 
-        indice = 0;       
-        printf("\tIngrese el tipo de CPU que posee la PC");
-        puts("1 --> Intel");
-        puts("2 --> AMD");
-        puts("3 --> Celeron");
-        puts("4 --> Athlon");
-        puts("5 --> Core");
-        puts("6 --> Pentium");
-        scanf("%d", &indice);
-        if (indice < 1 && indice > 6){
-            puts("VALOR FUERA DE RANGO");
-        }
-    } while (indice < 1 && indice > 6);
-    
     compu.tipo_cpu = strcpy(compu.tipo_cpu, tipos[indice]);
 
     return compu;
+}
+
+void mostrarLista(PC *lista){
+    printf("\t\t   LISTADO DE PCs\n ");
+    for (int i = 0; i < M; i++){
+        printf("\n\t\tFicha tecnica PC %d\n", i+1);
+        printf("CPU:\t\t\t%s\n",lista[i].tipo_cpu);
+        printf("Anio de fabricacion:\t%d\n",lista[i].anio);
+        printf("Nucleos:\t\t%d\n",lista[i].cantidad);
+        printf("Velocidad:\t\t%d GHz\n\n",lista[i].velocidad);
+    }
+}
+
+void masVieja(PC *lista){
+    int k = 0, comparacion = lista[0].anio; 
+    for (int i = 0; i < M; i++){
+        if (lista[i].anio < comparacion){
+            comparacion = lista[i].anio;
+            k = i;
+        }
+    }
+    puts("...COMPUTADORA MAS VIEJA...");
+    printf("\n\t\tFicha tecnica PC\n");
+    printf("CPU:\t\t\t%s\n",lista[k].tipo_cpu);
+    printf("Anio de fabricacion:\t%d\n",lista[k].anio);
+    printf("Nucleos:\t\t%d\n",lista[k].cantidad);
+    printf("Velocidad:\t\t%d GHz\n\n",lista[k].velocidad);
+}
+
+void cuchau(PC *lista){
+    int k = 0, comparacion = lista[0].velocidad; 
+    for (int i = 0; i < M; i++){
+        if (lista[i].velocidad > comparacion){
+            comparacion = lista[i].velocidad;
+            k = i;
+        }
+    }
+    puts("...COMPUTADORA MAS VELOZ...");
+    printf("\n\t\tFicha tecnica PC\n");
+    printf("CPU:\t\t\t%s\n",lista[k].tipo_cpu);
+    printf("Anio de fabricacion:\t%d\n",lista[k].anio);
+    printf("Nucleos:\t\t%d\n",lista[k].cantidad);
+    printf("Velocidad:\t\t%d GHz\n\n",lista[k].velocidad);
 }
